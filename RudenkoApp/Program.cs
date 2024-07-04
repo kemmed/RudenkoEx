5fb7fb03d483821af8b5361170af039ea958e3bb
kemmed <kemmed28@mail.ru>
2024-07-04 10:25:15 +0300

    Add routs

--- RudenkoApp/Program.cs
+++ RudenkoApp/Program.cs
@@ -6,7 +6,25 @@ namespace RudenkoApp
     {
         static void Main(string[] args)
         {
-            
+            TrolleybusStation trolleybusStation = new TrolleybusStation();
+            int count = 0;
+            bool flag = true;
+            while (flag)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите число маршрутов, которое хотите добавить: ");
+                if (!int.TryParse(Console.ReadLine(), out count) && count > 0)
+                {
+                    Console.WriteLine("Введите целое положительное число!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    flag = false;
+                }
+            }
+            trolleybusStation.Add(count);
         }
     }
 }
--- RudenkoApp/TrolleybusStation.cs
+++ RudenkoApp/TrolleybusStation.cs
@@ -8,9 +8,65 @@ namespace RudenkoApp
     {
         public List<Route> Routes = new List<Route>();
 
-        public void Add()
+        public void Add(int size)
         {
-
+            for (int i = 0; i < size; i++)
+            {
+                string num = "";
+                int count = 0;
+                int time = 0 ;
+                bool flag = true;
+                while (flag)
+                {
+                    Console.Clear();
+                    Console.Write($"Введите номер для {i + 1} маршрута: ");
+                    num = Console.ReadLine();
+                    if (num.Replace(" ", "") == "")
+                    {
+                        Console.WriteLine("Введите номер!");
+                        Console.ReadKey();
+                    }
+                    else
+                        flag = false;
+                }
+                flag = true;
+                while (flag)
+                {
+                    Console.Write($"Введите кол-во остановок для {i + 1} маршрута: ");
+                    if (!(int.TryParse(Console.ReadLine(), out count) && count > 0))
+                    {
+                        Console.WriteLine("Введите целое положительное число!");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    else
+                        flag = false;
+                }
+                flag = true;
+                while (flag)
+                {
+                    Console.Write($"Введите время в пути (мин.) для {i + 1} маршрута: ");
+                    if (!(int.TryParse(Console.ReadLine(), out time) && time > 0))
+                    {
+                        Console.WriteLine("Введите целое положительное число!");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        flag = false;
+                        Route route = new Route
+                        {
+                            NumRoute = num,
+                            CountStop = count,
+                            TimeRoute = time
+                        };
+                        Routes.Add(route);
+                    }
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"Вы успешно добавили маршруты в кол-ве {Routes.Count}");
         }
 
         public void Sort()
