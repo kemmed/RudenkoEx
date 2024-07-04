39a0b054eb420ecbbfbd1ab8698dc1cc2cb39991
kemmed <kemmed28@mail.ru>
2024-07-04 09:38:05 +0300

    Add class Route

---
+++ RudenkoApp/Route.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RudenkoApp
+{
+    public class Route
+    {
+        public string NumRoute;
+        public int CountStop;
+        public int TimeRoute;
+
+    }
+}
